bbf95763b23bbc4c04d380d891564e653a8d69c1
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2019-12-30 23:43:05 -0800

    implementing authentication

--- src/app.js
+++ src/app.js
@@ -5,23 +5,37 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
+app.use(cors());
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
+if (!isProduction) {
+  app.use(errorHandler());
+}
+
+// configure mongoose
 mongoose.connect('mongodb://localhost:27017/waivers_local', {useNewUrlParser: true, useUnifiedTopology: true}).then(
     () => {console.log("connection successful")},
     // eslint-disable-next-line no-unused-vars
     _err => {console.log("connection failed")}
 );
 mongoose.set('debug', true);
+
+// Models & Routes
+require('./models/Users');
+
+// Passport Config (must be below models)
+require('./config/passport');
 
 if (!isProduction) {
     app.use((err, req, res) => {
--- src/models/Users.js
+++ src/models/Users.js
@@ -1,4 +1,6 @@
+/* global require */
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 
 var Schema = mongoose.Schema;
 
@@ -16,3 +18,25 @@ UsersSchema.methods.setPassword = function(password) {
 UsersSchema.methods.validatePassword = function(password) {
     return this.password === password;
 }
+
+UsersSchema.methods.generateJWT = function() {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        username: this.username,
+        id: this._id,
+        exp: parseInt(expirationDate.getTime() / 1000, 10)
+    }, 'secret');
+};
+
+UsersSchema.methods.toAuthJSON = function() {
+    return {
+        _id: this._id,
+        username: this.username,
+        token: this.generateJWT()
+    };
+};
+
+mongoose.model('Users', UsersSchema);

b66d8db6016bf5118e832bed0e67aa499ad72727
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-11-28 16:28:11 -0800

    remove connection from app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 /* eslint-disable no-undef */
 /*global require*/
+// eslint-disable-next-line no-unused-vars
 const mongoose = require('mongoose');
-// configure mongoose
-mongoose.connect('mongodb://localhost:27017/waivers_local', { useNewUrlParser: true, useUnifiedTopology: true }).then(
-    () => { console.log("connection successful") },
-    // eslint-disable-next-line no-unused-vars
-    _err => { console.log("connection failed") }
-);
-mongoose.set('debug', true);
 
 const express = require('express');
 const bodyParser = require('body-parser');

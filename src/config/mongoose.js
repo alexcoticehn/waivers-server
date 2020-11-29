c7b7b570a9ad6ba9b0e20ceb91832a43dc20d356
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-11-28 16:26:44 -0800

    edits to connection file

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,11 +1,7 @@
 /*global require*/
 const mongoose = require('mongoose');
 // configure mongoose
-mongoose.connect('mongodb://localhost:27017/waivers_local', { useNewUrlParser: true, useUnifiedTopology: true }).then(
-    () => { console.log("connection successful") },
-    // eslint-disable-next-line no-unused-vars
-    _err => { console.log("connection failed") }
-);
+mongoose.connect('mongodb://localhost:27017/waivers_local', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('debug', true);
 // eslint-disable-next-line no-undef
 module.exports = mongoose;

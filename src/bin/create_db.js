7aa9215c26c0fd1fc2f4d2431cba86e17544610c
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-12-10 18:37:56 -0800

    finish create users script

--- src/bin/create_db.js
+++ src/bin/create_db.js
@@ -5,6 +5,14 @@ const mongoose = require('mongoose');
 require('../models/Users');
 const UsersModel = mongoose.model('Users');
 
+UsersModel.deleteMany()
+    .catch((err) => {
+        console.log(err);
+    })
+    .then((res) => {
+        console.log(res);
+    });
+
 const Dimitri = {
     username: "dfilipovic",
     firstname: "Dimitri",
@@ -12,7 +20,74 @@ const Dimitri = {
     password: "123456789"
 };
 
-UsersModel.create(Dimitri, function (err, res) {
-    if (err) console.log(err);
-    else console.log("Created Dimitri: " + res);
-});
+const Harrison = {
+    username: "hbrown",
+    firstname: "Harrison",
+    lastname: "Brown",
+    password: "123456789"
+};
+
+const Alex = {
+    username: "acoticehn",
+    firstname: "Alex",
+    lastname: "Cotic-Ehn",
+    password: "123456789"
+};
+
+const Chris = {
+    username: "clittomericzky",
+    firstname: "Chris",
+    lastname: "Littomericzky",
+    password: "123456789"
+};
+
+const Eric = {
+    username: "ewallin",
+    firstname: "Eric",
+    lastname: "Wallin",
+    password: "123456789"
+};
+
+const Ian = {
+    username: "icarter",
+    firstname: "Ian",
+    lastname: "Carter",
+    password: "123456789"
+};
+
+const Stefan = {
+    username: "skalaba",
+    firstname: "Stefan",
+    lastname: "Kalaba",
+    password: "123456789"
+};
+
+const Jimmy = {
+    username: "jghuman",
+    firstname: "Jimmy",
+    lastname: "Ghuman",
+    password: "123456789"
+};
+
+const Nathan = {
+    username: "nalvarez",
+    firstname: "Nathan",
+    lastname: "Alvarez",
+    password: "123456789"
+};
+
+const Peter = {
+    username: "pbohdal",
+    firstname: "Peter",
+    lastname: "Bohdal",
+    password: "123456789"
+};
+
+UsersModel.insertMany([Dimitri, Alex, Harrison, Chris, Eric, Ian, Stefan, Jimmy, Nathan, Peter])
+    .then((doc) => {
+        console.log(doc);
+        process.exit();
+    })
+    .catch((err) => {
+        console.log(err);
+    });

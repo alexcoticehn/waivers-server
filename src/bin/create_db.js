1f507159906dcdb869814d5fec9e29b6b092e1d7
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-12-14 20:51:59 -0800

    restructure create db script to use async hash

--- src/bin/create_db.js
+++ src/bin/create_db.js
@@ -8,84 +8,17 @@ const mongoose = require('mongoose');
 const SaltRounds = require('../config/passport');
 const UsersModel = mongoose.model('Users');
 
-UsersModel.deleteMany()
+function clearUsers() {
+    UsersModel.deleteMany()
     .catch((err) => {
         console.log(err);
         process.exit();
     })
     .then(() => {});
+}
 
-const Dimitri = {
-    username: "dfilipovic",
-    firstname: "Dimitri",
-    lastname: "Filipovic",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Harrison = {
-    username: "hbrown",
-    firstname: "Harrison",
-    lastname: "Brown",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Alex = {
-    username: "acoticehn",
-    firstname: "Alex",
-    lastname: "Cotic-Ehn",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Chris = {
-    username: "clittomericzky",
-    firstname: "Chris",
-    lastname: "Littomericzky",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Eric = {
-    username: "ewallin",
-    firstname: "Eric",
-    lastname: "Wallin",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Ian = {
-    username: "icarter",
-    firstname: "Ian",
-    lastname: "Carter",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Stefan = {
-    username: "skalaba",
-    firstname: "Stefan",
-    lastname: "Kalaba",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Jimmy = {
-    username: "jghuman",
-    firstname: "Jimmy",
-    lastname: "Ghuman",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Nathan = {
-    username: "nalvarez",
-    firstname: "Nathan",
-    lastname: "Alvarez",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-const Peter = {
-    username: "pbohdal",
-    firstname: "Peter",
-    lastname: "Bohdal",
-    password: bcrypt.hashSync("123456789", SaltRounds)
-};
-
-UsersModel.insertMany([Dimitri, Alex, Harrison, Chris, Eric, Ian, Stefan, Jimmy, Nathan, Peter])
+function createUsers(users_to_create) {
+    UsersModel.insertMany(users_to_create)
     .then(() => {
         process.exit();
     })
@@ -93,3 +26,85 @@ UsersModel.insertMany([Dimitri, Alex, Harrison, Chris, Eric, Ian, Stefan, Jimmy,
         console.log(err);
         process.exit();
     });
+}
+
+async function runScript() {
+    
+    clearUsers();
+
+    const Dimitri = {
+        username: "dfilipovic",
+        firstname: "Dimitri",
+        lastname: "Filipovic",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Harrison = {
+        username: "hbrown",
+        firstname: "Harrison",
+        lastname: "Brown",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Alex = {
+        username: "acoticehn",
+        firstname: "Alex",
+        lastname: "Cotic-Ehn",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Chris = {
+        username: "clittomericzky",
+        firstname: "Chris",
+        lastname: "Littomericzky",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Eric = {
+        username: "ewallin",
+        firstname: "Eric",
+        lastname: "Wallin",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Ian = {
+        username: "icarter",
+        firstname: "Ian",
+        lastname: "Carter",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Stefan = {
+        username: "skalaba",
+        firstname: "Stefan",
+        lastname: "Kalaba",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Jimmy = {
+        username: "jghuman",
+        firstname: "Jimmy",
+        lastname: "Ghuman",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Nathan = {
+        username: "nalvarez",
+        firstname: "Nathan",
+        lastname: "Alvarez",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const Peter = {
+        username: "pbohdal",
+        firstname: "Peter",
+        lastname: "Bohdal",
+        password: await bcrypt.hash("123456789", SaltRounds)
+    };
+
+    const users_to_create = [Dimitri, Harrison, Alex, Chris, Stefan, Eric, Ian, Peter, Jimmy, Nathan];
+
+    createUsers(users_to_create);
+}
+
+runScript();

87e951dda75f6e619bf27a2be1ba6da22fd99997
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-12-19 19:46:54 -0800

    use env variables for db connection

--- src/config/mongoose/mongoose.js
+++ src/config/mongoose/mongoose.js
@@ -1,7 +1,9 @@
 /*global require*/
+require('dotenv').config();
 const mongoose = require('mongoose');
 // configure mongoose
-mongoose.connect('mongodb://localhost:27017/waivers_local', { useNewUrlParser: true, useUnifiedTopology: true });
+// eslint-disable-next-line no-undef
+mongoose.connect(process.env.DB_HOST_DEV + process.env.DB_NAME_DEV, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('debug', true);
 // eslint-disable-next-line no-undef
 module.exports = mongoose;

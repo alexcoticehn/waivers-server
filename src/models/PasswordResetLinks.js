b7be3ee80c1a245261d7f30574c33867c464afc0
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-12-22 19:56:05 -0800

    switch order

--- src/models/PasswordResetLinks.js
+++ src/models/PasswordResetLinks.js
@@ -6,11 +6,11 @@ var Schema = mongoose.Schema;
 // Define schema
 const LinksSchema = new Schema({
   token: String,
+  tokenExpires: Date,
   user_id: {
       type: Schema.Types.ObjectId,
       ref: 'Users'
-  },
-  tokenExpires: Date
+  }
 });
 
 mongoose.model('Users', LinksSchema);

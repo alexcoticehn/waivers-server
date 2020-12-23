66ac950252e5b0671c2c644a98d69d0d69be1020
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-12-22 19:52:23 -0800

    create password reset model

---
+++ src/models/PasswordResetLinks.js
@@ -0,0 +1,13 @@
+/* global require */
+const mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// Define schema
+const LinksSchema = new Schema({
+  token: String,
+  user_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Users'
+  }
+});

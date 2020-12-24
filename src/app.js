5547bc1fdcac90db1d7a8e7df5d7f4c85bf2bb49
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2020-12-23 20:20:22 -0800

    remove unused packages from app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@
 /* eslint-disable no-undef */
 /*global require*/
 // eslint-disable-next-line no-unused-vars
-require('./config/mongoose/mongoose');
-const mongoose = require('mongoose');
+const mongoose = require('./config/mongoose/mongoose');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const session = require('express-session');
-const path = require('path');
 const cors = require('cors');
 const errorHandler = require('errorhandler');
 
@@ -18,10 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'secret', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 if (!isProduction) {
   app.use(errorHandler());

2a8a58b60410146ceaab6df8def333a84c01adff
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-05 19:53:48 -0800

    remove lint comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
-/* global require */
 const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./api/users'));
 
-// eslint-disable-next-line no-undef
 module.exports = router;

c492f98c117547ed9598a6835f8c8b229e659f67
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-14 21:09:44 -0800

    put test inside container

--- src/tests/routes/users.test.js
+++ src/tests/routes/users.test.js
@@ -1,13 +1,15 @@
 const request = require("supertest");
 const app = require('../../app');
 
-test('Successful Login Test', () => {
-    return request(app)
-        .post('/api/users/login', {
-            username: 'dfilipovic',
-            password: '123456789'
-        })
-        .then((res) => {
-            console.log(res.status);
-        });
-})
+describe('Login Tests', () => {
+    test('Successful Login Test', () => {
+        return request(app)
+            .post('/api/users/login', {
+                username: 'dfilipovic',
+                password: '123456789'
+            })
+            .then((res) => {
+                console.log(res.status);
+            });
+    })
+});

03f28c2da4e1b779a233306f73e69d062fed522c
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-16 21:03:14 -0800

    add test email config

--- src/config/nodemailer/nodemailer.config.js
+++ src/config/nodemailer/nodemailer.config.js
@@ -1,20 +1,33 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        type: 'OAuth2',
-        user: process.env.JAILORS_EMAIL_ADDRESS,
-        clientId: process.env.JAILORS_EMAIL_CLIENT_ID,
-        clientSecret: process.env.JAILORS_EMAIL_CLIENT_SECRET,
-        accessToken: process.env.JAILORS_EMAIL_ACCESS_TOKEN,
-        refreshToken: process.env.JAILORS_EMAIL_REFRESH_TOKEN,
-        tls: {
-            rejectUnauthorized: false
+var transporter;
+
+if (process.env.NODE_ENV === 'test') {
+    transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'cordie.wiegand43@ethereal.email',
+            pass: 'XsqYEsDzZHbFv9ecCY'
+        }   
+    });
+} else {
+    transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            user: process.env.JAILORS_EMAIL_ADDRESS,
+            clientId: process.env.JAILORS_EMAIL_CLIENT_ID,
+            clientSecret: process.env.JAILORS_EMAIL_CLIENT_SECRET,
+            accessToken: process.env.JAILORS_EMAIL_ACCESS_TOKEN,
+            refreshToken: process.env.JAILORS_EMAIL_REFRESH_TOKEN,
+            tls: {
+                rejectUnauthorized: false
+            }
         }
-    }
-});
+    });
+}
 
 module.exports.transporter = transporter;

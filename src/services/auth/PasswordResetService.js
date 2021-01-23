111c3f6ca38c822e1bc9b1a1f779b5da4038601f
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-22 20:05:02 -0800

    add functions to check if password reset link is valid

--- src/services/auth/PasswordResetService.js
+++ src/services/auth/PasswordResetService.js
@@ -27,7 +27,43 @@ module.exports.createOrUpdatePasswordResetLink = async function(userId) {
     return resetLink;
 }
 
+/**
+ * Grab existing password reset link for this user if exists
+ * @param {ObjectId} userId 
+ */
 async function findExistingPasswordResetLink(userId) {
     const resetLink = await PasswordResetLinks.findOne({user_id: userId, pending: 1})
+    return resetLink;
+}
+
+/**
+ * 
+ * @param {String} token 
+ */
+module.exports.getUserIdFromPasswordResetToken = async function(token) {
+    const resetLink = await findPasswordResetLinkFromToken(token);
+    if (!isPasswordResetLinkValid(resetLink)) {
+        return false;
+    }
+    return resetLink.user_id;
+}
+
+/**
+ * Verifies PasswordResetLink object is still valid (pending and time not expired)
+ * @param {PasswordResetLink} resetLink 
+ */
+function isPasswordResetLinkValid(resetLink) {
+    if (resetLink) {
+        return (resetLink.tokenExpires > Date.now() && resetLink.pending);
+    }
+    return false;
+}
+
+/**
+ * Find password reset link (if exists) for given token
+ * @param {string} token 
+ */
+async function findPasswordResetLinkFromToken(token) {
+    const resetLink = await PasswordResetLinks.findOne({token: token});
     return resetLink;
 }

fd9d8574015de76098af4239f17f58567d2a1340
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-22 20:05:40 -0800

    add find user by id function

--- src/services/users/UsersService.js
+++ src/services/users/UsersService.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 const Users = mongoose.model('Users');
 
+/**
+ * 
+ * @param {String} userEmail 
+ */
 module.exports.findUserByEmail = async function(userEmail) {
     return await Users.findOne({email: userEmail});
 }
 
+/**
+ * 
+ * @param {String} username 
+ */
 module.exports.findUserByUsername = async function(username) {
     return await Users.findOne({username: username});
+}
+
+/**
+ * 
+ * @param {ObjectId} userId 
+ */
+module.exports.findUserById = async function(userId) {
+    return await Users.findById(userId);
 }

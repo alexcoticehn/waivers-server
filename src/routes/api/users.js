f3c6bbdb1282dd0955cc35a005dc62f679a16ce0
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-25 20:24:33 -0800

    add new patch endpoint

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -51,39 +51,20 @@ router.put('/reset/send', RequestController.verifyPasswordResetRequest, Password
  */
 router.get("/reset/verify", RequestController.verifyPasswordResetTokenRequest, PasswordResetController.verifyResetTokenValid);
 
-// PATCH reset password route (required, only authenticated users have access) commented out for now
-/*
-router.patch("/reset/password", Auth.required, (req, res) => {
-  const { body: { user } } = req;
-
-  let new_password = user.new_password;
-
-  if (!new_password) {
-    return res.status(status_codes.UNPROCESSABLE_ENTITY).json({
-      errors: {
-        new_password: "is required",
-      },
-    });
-  }
-
-  Users.findById(user.id)
-    .then((found_user) => {
-      if (!found_user) {
-        return res.status(status_codes.NOT_FOUND).json({
-          errors: {
-            user: "not found",
-          },
-        });
-      }
-
-      found_user.password = new_password;
-      found_user.save();
-
-      return res.sendStatus(status_codes.OK);
-    }).catch((err) => {
-        return res.status(status_codes.NOT_FOUND).json(err);
-    })
-});
-*/
+/**
+ * PATCH route to reset a user's password
+ * Sample request:
+ * {
+ *  token: ...
+ *  id: ...
+ *  password: ...
+ * }
+ * 
+ * Sample response: (empty), Sets httpOnly cookie with JWT
+ * {
+ *  
+ * }
+ */
+router.patch("/reset/confirm", RequestController.verifyPasswordResetConfirmationRequest, PasswordResetController.resetPasswordConfirm);
 
 module.exports = router;

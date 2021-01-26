bbe45d0cd07b60fa419a5c3415b23117964a52e2
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-25 19:45:43 -0800

    add method to verify password reset confirmation requests

--- src/controllers/RequestController.js
+++ src/controllers/RequestController.js
@@ -8,7 +8,7 @@ const StatusCodes = require("../constants/StatusCodes");
  */
 module.exports.verifyLoginRequest = function(req, res, next) {
     if (!req.body.user || !req.body.user.username || !req.body.user.password) {
-        return res.status(StatusCodes.BAD_REQUEST).json({});
+        return res.status(StatusCodes.BAD_REQUEST).json();
     }
     next();
 }
@@ -21,7 +21,7 @@ module.exports.verifyLoginRequest = function(req, res, next) {
  */
 module.exports.verifyPasswordResetRequest = function(req, res, next) {
     if (!req.body.user || !req.body.user.email) {
-        return res.status(StatusCodes.BAD_REQUEST).json({});
+        return res.status(StatusCodes.BAD_REQUEST).json();
     }
     next()
 }
@@ -34,7 +34,20 @@ module.exports.verifyPasswordResetRequest = function(req, res, next) {
  */
 module.exports.verifyPasswordResetTokenRequest = function(req, res, next) {
     if (!req.body.token) {
-        return res.status(StatusCodes.BAD_REQUEST).json({});
+        return res.status(StatusCodes.BAD_REQUEST).json();
+    }
+    next();
+}
+
+/**
+ * Verifies that the password reset confirmation reques is sent in the correct format
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+module.exports.verifyPasswordResetConfirmationRequest = function(req, res, next) {
+    if (!req.body.token || !req.body.password || !req.body.id) {
+        return res.status(StatusCodes.BAD_REQUEST).json();
     }
     next();
 }

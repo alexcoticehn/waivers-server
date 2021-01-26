84183ad1d8818899b4a1b7be30868d6e29f862c7
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-01-25 19:25:13 -0800

    add sample requests and responses for existing routes

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -3,13 +3,52 @@ const UsersController = require('../../controllers/UsersController');
 const RequestController = require('../../controllers/RequestController');
 const PasswordResetController = require('../../controllers/PasswordResetController');
 
-// POST route to process login requests
+/**
+ * POST route to process login requests
+ * Sample request:
+ * {
+ *  user: {
+ *    username: ...
+ *    password: ...
+ *  }
+ * }
+ * 
+ * Sample response: (empty) Sets httpOnly cookie with JWT
+ * {
+ * }
+ */ 
 router.post("/login", RequestController.verifyLoginRequest, UsersController.userLogin);
 
-// POST route to create and send password reset link
+/**
+ * POST route to create and send password reset link
+ * Sample request: 
+ * {
+ *  user: {
+ *    email: ...
+ *  }
+ * }
+ * 
+ * Sample response: 
+ * {
+ *  message: "Password reset email sent"
+ * }
+ */
 router.put('/reset/send', RequestController.verifyPasswordResetRequest, PasswordResetController.sendResetEmail);
 
-// GET route to check validity of password reset token
+/**
+ * GET route to check validity of password reset token
+ * Sample request:
+ * {
+ *  token: ...
+ * }
+ * 
+ * Sample response:
+ * {
+ *  firstname: ...
+ *  id: ...
+ *  token: ...
+ * }
+ */
 router.get("/reset/verify", RequestController.verifyPasswordResetTokenRequest, PasswordResetController.verifyResetTokenValid);
 
 // PATCH reset password route (required, only authenticated users have access) commented out for now

c8e9cdd57e62a422b681bab56c1f3804e0de73d3
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-02-04 21:29:55 -0800

    throw errors for bad requests

--- src/controllers/RequestController.js
+++ src/controllers/RequestController.js
@@ -1,4 +1,5 @@
 const StatusCodes = require("../constants/StatusCodes");
+const {JailorsError} = require('../errors/JailorsError');
 
 /**
  * Verifies that a login request is sent in the correct format
@@ -8,7 +9,8 @@ const StatusCodes = require("../constants/StatusCodes");
  */
 module.exports.verifyLoginRequest = function(req, res, next) {
     if (!req.body.user || !req.body.user.username || !req.body.user.password) {
-        return res.status(StatusCodes.BAD_REQUEST).json();
+        const error = new JailorsError('Missing request parameter', StatusCodes.BAD_REQUEST);
+        next(error);
     }
     next();
 }
@@ -21,7 +23,8 @@ module.exports.verifyLoginRequest = function(req, res, next) {
  */
 module.exports.verifyPasswordResetRequest = function(req, res, next) {
     if (!req.body.user || !req.body.user.email) {
-        return res.status(StatusCodes.BAD_REQUEST).json();
+        const error = new JailorsError('Missing request parameter', StatusCodes.BAD_REQUEST);
+        next(error);
     }
     next()
 }
@@ -34,7 +37,8 @@ module.exports.verifyPasswordResetRequest = function(req, res, next) {
  */
 module.exports.verifyPasswordResetTokenRequest = function(req, res, next) {
     if (!req.body.token) {
-        return res.status(StatusCodes.BAD_REQUEST).json();
+        const error = new JailorsError('Missing request parameter', StatusCodes.BAD_REQUEST);
+        next(error);
     }
     next();
 }
@@ -47,7 +51,8 @@ module.exports.verifyPasswordResetTokenRequest = function(req, res, next) {
  */
 module.exports.verifyPasswordResetConfirmationRequest = function(req, res, next) {
     if (!req.body.token || !req.body.password || !req.body.id) {
-        return res.status(StatusCodes.BAD_REQUEST).json();
+        const error = new JailorsError('Missing request parameter', StatusCodes.BAD_REQUEST);
+        next(error);
     }
     next();
 }

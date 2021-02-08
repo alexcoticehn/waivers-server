0e339f32ccdb7669d8b8d38f73e19b8093ec7de4
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-02-07 21:54:15 -0800

    add debug package

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+const debug = require('debug')('app');
 
 const port = 3000;
-app.listen(port, () => console.log("App listening on port " + port));
+app.listen(port, () => debug("App listening on port " + port));

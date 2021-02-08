2aca9b7105941c4f76e4e1b20a0587a9c24f9eee
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-02-07 23:37:17 -0800

    add default to port heroku sets

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const debug = require('debug')('app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => debug("App listening on port " + port));

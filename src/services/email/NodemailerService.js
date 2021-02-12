ed96b2b320ae16328edfe3d4bb14f676b53b18b4
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-02-11 23:50:37 -0800

    edit nodemailer service to only use ethereal mail

--- src/services/email/NodemailerService.js
+++ src/services/email/NodemailerService.js
@@ -1,34 +1,15 @@
 const nodemailer = require('nodemailer');
 
 module.exports.getTransporter = function() {
-    let transporter;
-    if (process.env.NODE_ENV === 'test') {
-        transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.JAILORS_TEST_EMAIL_ADDRESS,
-                pass: process.env.JAILORS_TEST_EMAIL_PASSWORD
-            }   
-        });
-    } else {
-        transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            logger: true,
-            debug: true,
-            auth: {
-                type: 'OAuth2',
-                user: process.env.JAILORS_EMAIL_ADDRESS,
-                clientId: process.env.JAILORS_EMAIL_CLIENT_ID,
-                clientSecret: process.env.JAILORS_EMAIL_CLIENT_SECRET,
-                // had accessToken here before, see if this works better?
-                refreshToken: process.env.JAILORS_EMAIL_REFRESH_TOKEN
-            }
-        });
-    }
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.JAILORS_TEST_EMAIL_ADDRESS,
+            pass: process.env.JAILORS_TEST_EMAIL_PASSWORD
+        }   
+    });
 
     return transporter;
 }

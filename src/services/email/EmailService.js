8d08ad3bf650eca0768f7f603f4d7ac00cd95d31
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-02-11 23:23:07 -0800

    use send grid in prod

--- src/services/email/EmailService.js
+++ src/services/email/EmailService.js
@@ -1,12 +1,26 @@
 const nodemailer = require('./NodemailerService');
+const sendGrid = require('@sendgrid/mail');
 
 module.exports.sendEmail = async function(recipient, subject, body) {
-    let transporter = nodemailer.getTransporter();
-    let info = await transporter.sendMail({
-        from: `Sailor Jerry's League Office <${process.env.JAILORS_EMAIL_ADDRESS}>`,
-        to: recipient,
-        subject: subject,
-        text: body
-    })
+    let info;
+    if (process.env.NODE_ENV === 'production') {
+        sendGrid.setApiKey(process.env.SEND_GRID_API_KEY);
+        const message = {
+            to: recipient,
+            from: `Sailor Jerry's League Office <${process.env.JAILORS_EMAIL_ADDRESS}>`,
+            subject: subject,
+            text: body
+        }
+
+        info = await sendGrid.send(message);
+    } else {
+        let transporter = nodemailer.getTransporter();
+        info = await transporter.sendMail({
+            from: `Sailor Jerry's League Office <${process.env.JAILORS_EMAIL_ADDRESS}>`,
+            to: recipient,
+            subject: subject,
+            text: body
+        })
+    }
     return info;
 }

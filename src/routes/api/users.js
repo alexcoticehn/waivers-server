fd6fb6862b60705f0a8a0d50076e2949dafb7a65
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-02-13 14:22:03 -0800

    send success response

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const UsersController = require('../../controllers/UsersController');
 const RequestController = require('../../controllers/RequestController');
 const PasswordResetController = require('../../controllers/PasswordResetController');
+const StatusCodes = require("../../constants/StatusCodes");
 
 /**
  * POST route to process login requests
@@ -70,6 +71,8 @@ router.patch("/reset/confirm", RequestController.verifyPasswordResetConfirmation
 /**
  * GET route to verify if a user's token is still valid
  */
-router.get("/token/verify", UsersController.verifyJWT);
+router.get("/token/verify", UsersController.verifyJWT, (req, res, _next) => {
+    return res.status(StatusCodes.OK).json();
+});
 
 module.exports = router;

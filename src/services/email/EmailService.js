7fa9f56ab2c79a66bde6e7aa8e6b84c34058bb0f
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-03-24 18:55:16 -0700

    add function to create password reset email body

--- src/services/email/EmailService.js
+++ src/services/email/EmailService.js
@@ -17,3 +17,12 @@ module.exports.sendEmail = async function(recipient, subject, body) {
     }
     return info;
 }
+
+/**
+ * Returns body of the password reset email to be sent with url to follow
+ */
+module.exports.getResetPasswordEmailBody = function(hostname, token) {
+    return "A password reset has been triggered on the account associated with this email. Please follow the link below.\n" +
+        "The link is valid for 60 minutes from the time of request. Once this link expires, you will have to request a new reset link. \n\n" +
+        hostname + '/reset/' + token;
+}

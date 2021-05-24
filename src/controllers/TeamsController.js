4dd94cfc0fa291801a6e9be2788f283fac6b418c
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-23 21:20:31 -0700

    edit method to handle errors and grab all teams, must fill in response with data

--- src/controllers/TeamsController.js
+++ src/controllers/TeamsController.js
@@ -1,8 +1,17 @@
 const StatusCodes = require("../constants/StatusCodes");
+const TeamsService = require('../services/teams/TeamsService');
+const { JailorsError } = require('../errors/JailorsError');
 
 /**
  * Method to handle get request for all teams
  */
-module.exports.getTeams = function(req, res) {
-    return res.status(StatusCodes.OK).json();
+module.exports.getTeams = function(req, res, next) {
+    TeamsService.findAllTeams()
+        .then((teams) => {
+            return res.status(StatusCodes.OK).json();  
+        })
+        .catch(() => {
+            const error = new JailorsError("An error occurred, please try again", StatusCodes.INTERNAL_SERVER_ERROR);
+            next(error);
+        })
 }

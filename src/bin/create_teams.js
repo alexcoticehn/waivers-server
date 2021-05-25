a544961031acf0f65153d374257eb2c42bfb7cd5
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-24 20:09:01 -0700

    edit create teams script

--- src/bin/create_teams.js
+++ src/bin/create_teams.js
@@ -4,13 +4,13 @@
 require('dotenv').config();
 require('../models/Users');
 require('../models/Teams');
-const argv = require('minimist')(process.argv.slice(2));
+// const argv = require('minimist')(process.argv.slice(2));
 const mongoose = require('mongoose');
 const TeamsModel = mongoose.model('Teams');
 const UsersService = require('../services/users/UsersService');
 
 async function connectDB() {
-    await mongoose.connect(process.env.DB_HOST_DEV + argv.db, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.DB_CLOUD_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 async function disconnectDB() {
@@ -21,67 +21,85 @@ async function clearTeams() {
     await TeamsModel.deleteMany();
 }
 
-async function createTeams(teams_to_create) {
-    await TeamsModel.insertMany(teams_to_create);
+async function createTeam(team_to_create) {
+    return await TeamsModel.create(team_to_create);
 }
 
 async function runScript() {
     await connectDB();
     await clearTeams();
 
+    const Alex = await UsersService.findUserByUsername('acoticehn');
+    const Dim = await UsersService.findUserByUsername('dfilipovic');
+    const Harry = await UsersService.findUserByUsername('hbrown');
+    const Chris = await UsersService.findUserByUsername('clittomericzky');
+    const Eric = await UsersService.findUserByUsername('ewallin');
+    const Nathan = await UsersService.findUserByUsername('nalvarez');
+    const Peter = await UsersService.findUserByUsername('pbohdal');
+    const Jimmy = await UsersService.findUserByUsername('jghuman');
+    const Mike = await UsersService.findUserByUsername('mwinter');
+    const Ian = await UsersService.findUserByUsername('icarter');
+
+    const users = [Ian, Mike, Jimmy, Peter, Nathan, Eric, Chris, Harry, Dim, Alex];
+
     const DTMB = {
         name: "Don't Toews Me, Bro",
-        owner_id: (await UsersService.findUserByUsername('acoticehn'))._id
+        owner: Alex.id
     }
 
     const DBS = {
         name: "Don't Be Saad",
-        owner_id: (await UsersService.findUserByUsername('dfilipovic'))._id
+        owner: Dim.id
     }
 
     const HKH = {
         name: "Wheeler? I Hardly Knew Her!",
-        owner_id: (await UsersService.findUserByUsername('hbrown'))._id
+        owner: Harry.id
     }
 
     const DWM = {
         name: "Dude, Where's Makar?",
-        owner_id: (await UsersService.findUserByUsername('clittomericzky'))._id
+        owner: Chris.id
     }
 
     const DP = {
         name: "Double Pennertration",
-        owner_id: (await UsersService.findUserByUsername('ewallin'))._id
+        owner: Eric.id
     }
 
     const TGUD = {
         name: "Team TOO GOOD",
-        owner_id: (await UsersService.findUserByUsername('nalvarez'))._id
+        owner: Nathan.id
     }
 
     const BIGM = {
         name: "Big Money $$$",
-        owner_id: (await UsersService.findUserByUsername('pbohdal'))._id
+        owner: Peter.id
     }
 
     const MCJ = {
         name: "McJesus!",
-        owner_id: (await UsersService.findUserByUsername('jghuman'))._id
+        owner: Jimmy.id
     }
 
     const BP = {
         name: "Bustin' Pumpkins",
-        owner_id: (await UsersService.findUserByUsername('mwinter'))._id
+        owner: Mike.id
     }
 
     const WDC = {
         name: "The Wet Dick Carters",
-        owner_id: (await UsersService.findUserByUsername('icarter'))._id
+        owner: Ian.id
     }
 
     const teams_to_create = [WDC, BP, MCJ, BIGM, TGUD, DP, DWM, HKH, DBS, DTMB];
 
-    await createTeams(teams_to_create);
+
+    for (let i = 0; i < users.length; i++) {
+        const teamDoc = await createTeam(teams_to_create[i]);
+        await users[i].updateOne({ team: teamDoc.id });
+    }
+
     await disconnectDB();
     process.exit();
 }

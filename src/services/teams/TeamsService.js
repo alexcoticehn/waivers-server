8a3329512f4eb536512fce06374c4f3fa399360a
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-25 19:19:21 -0700

    edit getAllTeams method to return connected user info

--- src/services/teams/TeamsService.js
+++ src/services/teams/TeamsService.js
@@ -6,5 +6,5 @@ const Teams = mongoose.model('Teams');
  * @returns Array of all mongoose team objects (version excluded)
  */
 module.exports.getAllTeams = async function() {
-    return await Teams.find({}).select('-__v');
+    return await Teams.find({}).select('-__v').populate({ path: 'owner', select: 'firstname lastname _id'});
 }

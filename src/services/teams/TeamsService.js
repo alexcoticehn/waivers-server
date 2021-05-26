efed40407e2fcc741feb3a1c73f905158ab890ca
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-25 19:36:38 -0700

    update doc block

--- src/services/teams/TeamsService.js
+++ src/services/teams/TeamsService.js
@@ -3,7 +3,7 @@ const Teams = mongoose.model('Teams');
 
 /**
  * 
- * @returns Array of all mongoose team objects (version excluded)
+ * @returns Array of all mongoose team objects including connected owner info
  */
 module.exports.getAllTeams = async function() {
     return await Teams.find({}).select('-__v').populate({ path: 'owner', select: 'firstname lastname _id'});

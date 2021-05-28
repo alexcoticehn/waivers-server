049e9505f1bef495156c0a3f023558e455bec593
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-27 21:14:30 -0700

    add sample response for get standings route

--- src/routes/api/standings.js
+++ src/routes/api/standings.js
@@ -2,6 +2,20 @@ const router = require("express").Router();
 const AuthController = require('../../controllers/AuthController');
 const StandingsController = require('../../controllers/StandingsController');
 
+/**
+ * GET request to return all standings info for given year and/or team
+ * Sample response:
+ * {
+ *   standings: [
+ *     {
+ *       team: "tg45g5wby",
+ *       year: "54g6byeh",
+ *       position: 1,
+ *       points: 82
+ *     }
+ *   ]
+ * }
+ */
 router.get('', AuthController.verifyJWT, StandingsController.getStandings);
 
 module.exports = router;

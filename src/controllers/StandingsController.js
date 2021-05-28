f8b5326c7994db4ba579502f79fae647ee3fca26
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2021-05-27 20:48:57 -0700

    add doc block for get Teams endpoint

--- src/controllers/StandingsController.js
+++ src/controllers/StandingsController.js
@@ -1,15 +1,27 @@
+const { JailorsError } = require('../errors/JailorsError');
 const StandingsService = require('../services/teams/StandingsService');
 
-module.exports.getStandings = async function(req, res) {
+/**
+ * Method to handle GET standings requests
+ * Filters by year or team if parameters provided
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next
+ */
+module.exports.getStandings = async function(req, res, next) {
     let standings;
-    if (req.query.team && req.query.year) {
-        standings = await StandingsService.getStandingsByYearAndTeam(req.query.team, req.query.year);
-    } else if (req.query.team) {
-        standings = await StandingsService.getStandingsByTeam(req.query.team);
-    } else if (req.query.year) {
-        standings = await StandingsService.getStandingsByYear(req.query.year);
-    } else {
-        standings = await StandingsService.getAllStandings();
+    try {
+        if (req.query.team && req.query.year) {
+            standings = await StandingsService.getStandingsByYearAndTeam(req.query.team, req.query.year);
+        } else if (req.query.team) {
+            standings = await StandingsService.getStandingsByTeam(req.query.team);
+        } else if (req.query.year) {
+            standings = await StandingsService.getStandingsByYear(req.query.year);
+        } else {
+            standings = await StandingsService.getAllStandings();
+        }
+    } catch(err) {
+        next(new JailorsError('An error occurred. If the error persists, please contact your system administrator.'));
     }
 
     return res.json({

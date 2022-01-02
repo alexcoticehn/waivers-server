f124f08d7cb10afe98ea5c244aa86978dea90602
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2022-01-01 19:08:28 -0800

    edit constants

--- src/constants/DraftPickConstants.js
+++ src/constants/DraftPickConstants.js
@@ -2,7 +2,7 @@ module.exports = {
     StatusOwned: 1,
     StatusDropped: 2,
     StatusExpired: 3,
-    StatusPending: 4,
+    StatusPending: 0,
     StringOwned: "owned",
     StringDropped: "dropped",
     StringExpired: "expired",

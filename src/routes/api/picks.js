12a57aa99548bf4c399cb89e6e490e6d9f7d699b
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2022-01-01 19:09:26 -0800

    add route for posting draft picks

---
+++ src/routes/api/picks.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const AuthController = require('../../controllers/AuthController');
+const RequestController = require('../../controllers/RequestController');
+const DraftPicksController = require('../../controllers/DraftPicksController');
+const { body } = require('express-validator');
+
+router.post('', AuthController.verifyJWT, body(['year', 'originalTeam']).notEmpty(), 
+    body(['pickingTeamName', 'pickingOwnerName', 'round', 'overall', 'contractYearsOriginal', 'contractYearsRemaining'], 'You must specify pickingTeamName, pickingOwnerName, round, overall, contractYearsOriginal, and contractYearsRemaining for requests that specify a player').if(body('player').notEmpty()).notEmpty(), 
+    body('status', 'Status must be a number value greater than 0 if player is specified').if(body('player').notEmpty()).isInt().custom((val) => val > 0),
+    RequestController.checkValidationErrors, DraftPicksController.saveDraftPick);
+
+module.exports = router;

da88d23dc8f5ac9bff6095d24d92418f52907d56
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2022-01-01 17:37:03 -0800

    add draft pick constants

---
+++ src/constants/DraftPickConstants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    StatusOwned: 1,
+    StatusDropped: 2,
+    StatusExpired: 3,
+    StatusPending: 4,
+    StringOwned: "owned",
+    StringDropped: "dropped",
+    StringExpired: "expired",
+    StringPending: "pending"
+}

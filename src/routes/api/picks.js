2bc0bae4f01fdca3c5c52fd2b8acc1ce1eaf4d7c
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2022-01-02 17:28:36 -0800

    add tests for post draft picks route

--- src/routes/api/picks.js
+++ src/routes/api/picks.js
@@ -5,7 +5,7 @@ const DraftPicksController = require('../../controllers/DraftPicksController');
 const { body } = require('express-validator');
 
 router.post('', AuthController.verifyJWT, body(['year', 'originalTeam']).notEmpty(), 
-    body(['pickingTeamName', 'pickingOwnerName', 'round', 'overall', 'contractYearsOriginal', 'contractYearsRemaining'], 'You must specify pickingTeamName, pickingOwnerName, round, overall, contractYearsOriginal, and contractYearsRemaining for requests that specify a player').if(body('player').notEmpty()).notEmpty(), 
+    body(['pickingTeamName', 'pickingOwnerName', 'round', 'overall', 'contractYearsOriginal', 'contractYearsRemaining']).if(body('player').notEmpty()).notEmpty(), 
     body('status', 'Status must be a number value greater than 0 if player is specified').if(body('player').notEmpty()).isInt().custom((val) => val > 0),
     RequestController.checkValidationErrors, DraftPicksController.saveDraftPick);
 
---
+++ src/tests/routes/picks/draftPicks.test.js
@@ -0,0 +1,94 @@
+const app = require('../../../app');
+const supertest = require("supertest");
+const request = supertest(app);
+const mongoose = require('mongoose');
+const AuthService = require('../../../services/auth/AuthService');
+const PassportConstants = require('../../../constants/PassportConstants');
+const StatusCodes = require('../../../constants/StatusCodes');
+const crypto = require('crypto');
+const DraftPicks = mongoose.model("DraftPicks");
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.DB_HOST_DEV + 'jailors_players_test', { useNewUrlParser: true, useUnifiedTopology: true });
+})
+
+afterAll(async () => {
+    await mongoose.disconnect();
+})
+
+describe('Post Draft Picks Tests', () => {
+    const token = AuthService.generateJWT('alex', crypto.randomBytes(24).toString('hex'));
+    DraftPicks.deleteMany({});
+
+    test('Invalid Request - Missing Year', async () => {
+        request.post('/jailors/api/picks')
+            .set('Cookie', [`${PassportConstants.TokenCookie}=${token}`])
+            .send({
+                originalTeam: "61d24b1f925d775745e2f97f"
+            })
+            .then((res) => {
+                expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+            })
+    })
+
+    test('Invalid Request - Missing Round', async () => {
+        request.post('/jailors/api/picks')
+            .set('Cookie', [`${PassportConstants.TokenCookie}=${token}`])
+            .send({
+                originalTeam: "61d24b1f925d775745e2f97f",
+                year: "61d24dec70a75cbb7ff05337",
+                player: "61a95c978be9093a2a514562",
+                pickingTeamName: "Test Team Name",
+                pickingOwnerName: "Test Owner Name",
+                overall: 23,
+                contractYearsOriginal: 3,
+                contractYearsRemaining: 0,
+                status: 1
+            })
+            .then((res) => {
+                expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+                expect(res.body.errors).toHaveLength(1);
+            })
+    })
+
+    test('Invalid Request - Bad Status Value', async () => {
+        request.post('/jailors/api/picks')
+            .set('Cookie', [`${PassportConstants.TokenCookie}=${token}`])
+            .send({
+                originalTeam: "61d24b1f925d775745e2f97f",
+                year: "61d24dec70a75cbb7ff05337",
+                player: "61a95c978be9093a2a514562",
+                pickingTeamName: "Test Team Name",
+                pickingOwnerName: "Test Owner Name",
+                overall: 23,
+                contractYearsOriginal: 3,
+                contractYearsRemaining: 0,
+                round: 3,
+                status: 0
+            })
+            .then((res) => {
+                expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+                expect(res.body.errors).toHaveLength(1);
+                expect(res.body.errors[0].msg).toBe('Status must be a number value greater than 0 if player is specified');
+            })
+    })
+
+    test('Invalid Request - Multiple Validation Errors', async () => {
+        request.post('/jailors/api/picks')
+            .set('Cookie', [`${PassportConstants.TokenCookie}=${token}`])
+            .send({
+                originalTeam: "61d24b1f925d775745e2f97f",
+                year: "61d24dec70a75cbb7ff05337",
+                player: "61a95c978be9093a2a514562",
+                pickingTeamName: "Test Team Name",
+                pickingOwnerName: "Test Owner Name",
+                overall: 23,
+                contractYearsOriginal: 3,
+                status: 1
+            })
+            .then((res) => {
+                expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
+                expect(res.body.errors).toHaveLength(2);
+            })
+    })
+})

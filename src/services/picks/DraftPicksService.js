efd6f59497b2e787a0fe7aec7890b0ce8eb4f87d
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2022-01-04 21:43:45 -0800

    add doc blocks

--- src/services/picks/DraftPicksService.js
+++ src/services/picks/DraftPicksService.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const DraftPicks = mongoose.model('DraftPicks');
 
+/**
+ * 
+ * @param {ObjectId} year 
+ * @param {ObjectId} originalTeam 
+ * @param {ObjectId} pickingTeam 
+ * @param {ObjectId} currentTeam 
+ * @param {ObjectId} player 
+ * @param {String} pickingTeamName 
+ * @param {String} pickingOwnerName 
+ * @param {Number} round 
+ * @param {Number} overall 
+ * @param {Number} contractYearsOriginal 
+ * @param {Number} contractYearsRemaining 
+ * @param {Number} status 
+ * @param {Number} timesExtended 
+ * @returns 
+ */
 module.exports.saveDraftPickRecord = async function(year, originalTeam, pickingTeam, currentTeam, player, pickingTeamName, pickingOwnerName, round, overall, contractYearsOriginal, contractYearsRemaining, status, timesExtended) {
     const pick = await DraftPicks.create({
         year: year,
--- src/services/sanitizers/SanitizationService.js
+++ src/services/sanitizers/SanitizationService.js
@@ -1,5 +1,10 @@
 const { ObjectId } = require('mongodb');
 
+/**
+ * 
+ * @param {ObjectId} objectId 
+ * @returns Returns a MongoDb ObjectId if a string is provided, undefined otherwise
+ */
 module.exports.sanitizeObjectIdString = function(objectId) {
     let sanitized_value;
     objectId ? sanitized_value = new ObjectId(objectId) : sanitized_value = undefined; 

c2028a532993a5b1e7f8f6223615709c13d9e215
Alex Cotic-Ehn <alex.cotic.ehn@gmail.com>
2022-03-31 15:20:37 -0700

    add doc block for post picks route

--- src/routes/api/picks.js
+++ src/routes/api/picks.js
@@ -5,6 +5,27 @@ const DraftPicksController = require('../../controllers/DraftPicksController');
 const SanitizationService = require('../../services/sanitizers/SanitizationService');
 const { body } = require('express-validator');
 
+/**
+ * POST route to add new prospect draft picks
+ * Sample Request: 
+ * {
+ *      originalTeam: "61d24b1f925d775745e2f97f",
+ *      year: "61d24dec70a75cbb7ff05337",
+ *      pickingTeam: "61d24b1f925d775745e2f97f",
+ *      round: 1,
+ *      currentTeam: "61d24b1f925d775745e2f982",
+ *      overall: 1,
+ *      player: "61a95c978be9093a2a514563",
+ *      status: 1,
+ *      pickingTeamName: "Test Team Name",
+ *      pickingOwnerName: "Test Name",
+ *      contractYearsOriginal: 3,
+ *      contractYearsRemaining: 2,
+ *      timesExtended: 0
+ * }
+ * 
+ *  Sample Response: Same as request but with _id
+ */
 router.post('', AuthController.verifyJWT, body(['year', 'originalTeam']).notEmpty(), 
     body(['pickingTeamName', 'pickingOwnerName', 'round', 'overall', 'contractYearsOriginal', 'contractYearsRemaining']).if(body('player').notEmpty()).notEmpty(), 
     body('status', 'Status must be a number value greater than 0 if player is specified').if(body('player').notEmpty()).isInt().custom((val) => val > 0),
